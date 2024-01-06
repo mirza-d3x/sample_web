520f4995e6af648f64c0b1600e43bfba0d1ef192
Mirza <mohammedmirsa010@gmail.com>
2024-01-06 17:17:28 +0530

    sample page created

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('startButton').addEventListener('click', function () {
+    document.getElementById('welcomeText').innerText = 'Start Button Clicked!';
+});
+
+document.getElementById('doneButton').addEventListener('click', function () {
+    document.getElementById('welcomeText').innerText = 'Done Button Clicked!';
+});
