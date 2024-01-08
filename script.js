60b792b8409f5184666b84e3b8f72b69d8f1efb2
Mirza <mohammedmirsa010@gmail.com>
2024-01-08 12:18:10 +0530

    web page completed

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-document.getElementById('startButton').addEventListener('click', function () {
-    document.getElementById('welcomeText').innerText = 'Start Button Clicked!';
-});
+// document.getElementById('startButton').addEventListener('click', function () {
+//     document.getElementById('welcomeText').innerText = 'Start Button Clicked!';
+//     window.flutter.postMessage("startButtonClicked");
+// });
 
-document.getElementById('doneButton').addEventListener('click', function () {
-    document.getElementById('welcomeText').innerText = 'Done Button Clicked!';
-});
+// document.getElementById('doneButton').addEventListener('click', function () {
+//     document.getElementById('welcomeText').innerText = 'Done Button Clicked!';
+//     window.flutter.postMessage("doneButtonClicked");
+// });
