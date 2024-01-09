0de8c6f587cd5f00defc9290a4d7538096a3de19
Mirza <mohammedmirsa010@gmail.com>
2024-01-09 16:20:13 +0530

    click issue fixed

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-document.getElementById('startButton').addEventListener('buttonClick', function () {
+document.getElementById('startButton').addEventListener('click', function () {
+    window.postMessage("StartButton")
     document.getElementById('welcomeText').innerText = 'Start Button Clicked!';
 });
 
-document.getElementById('doneButton').addEventListener('buttonClick', function () {
+document.getElementById('doneButton').addEventListener('click', function () {
+    window.postMessage("DoneButton");
     document.getElementById('welcomeText').innerText = 'Done Button Clicked!';
 });
