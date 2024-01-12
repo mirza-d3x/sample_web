9b182b854f57ed816e62fc0db8fad8ab9d8ed360
Mirza <mohammedmirsa010@gmail.com>
2024-01-12 12:16:59 +0530

    log message added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.getElementById('startButton').addEventListener('click', function () {
     window.postMessage("StartButton")
+    console.log("StartButton");
     document.getElementById('welcomeText').innerText = 'Start Button Clicked!';
 });
 
 document.getElementById('doneButton').addEventListener('click', function () {
     window.postMessage("DoneButton");
+    console.log("DoneButton");
     document.getElementById('welcomeText').innerText = 'Done Button Clicked!';
 });
